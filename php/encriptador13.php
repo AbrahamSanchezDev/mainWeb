ad798c89c11e3dc70bcd47a0a0ae8c9301314468
Abraham Sanchez <abraham_gto@hotmail.com>
2023-08-05 12:35:12 -0600

    desincriptado agregado

--- php/encriptador13.php
+++ php/encriptador13.php
@@ -15,8 +15,34 @@ function rot_13char($char)
         return $char;
     }
 }
+function des_rot_13char($char)
+{
+    $upperCase = range('A', 'Z');
+    $lowerCase = range('a', 'z');
 
+    if (in_array($char, $upperCase)) {
+        $index = (array_search($char, $upperCase) - 13 + 26) % 26;
+        return $upperCase[$index];
+    } else if (in_array($char, $lowerCase)) {
+        $index = (array_search($char, $lowerCase) - 13 + 26) % 26;
+        return $lowerCase[$index];
+    } else {
+        return $char;
+    }
+}
 function rot13($muchoTexto)
+{
+    //Mensaje completo
+    $encriptado = "";
+    //cuantos caracteres hay en el msj
+    $longitud = strlen($muchoTexto);
+    for ($i = 0; $i < $longitud; $i++) {
+        $char = $muchoTexto[$i];
+        $encriptado .= des_rot_13char($char);
+    }
+    return $encriptado;
+}
+function des_rot13($muchoTexto)
 {
     //Mensaje completo
     $encriptado = "";
@@ -31,7 +57,10 @@ function rot13($muchoTexto)
 
 $miMensaje = "cualquier cosa jojo";
 
-echo rot13($miMensaje);
+$encriptada = rot13($miMensaje);
+echo $encriptada;
 
+echo "  -  ";
+echo des_rot13($encriptada);
 
 ?>
